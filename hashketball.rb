5b7ad489beb739f391d0514b0f2bf95c4ad9b448
Andy Del Valle <andydelvalle89@gmail.com>
2020-04-11 23:46:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,7 @@ def game_hash
         :assists => 12,
         :steals => 3,
         :blocks => 1,
-        :slam_dunks => 1
+        :slam_dunks => 1home
       },{
         :player_name =>"Reggie Evans",
         :number => 30,
